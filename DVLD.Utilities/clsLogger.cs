4f1e2944a8db9dce2a29ce5274ff2e432338d2ac
Mohammed Salah <27531732+MohammedSalahCode@users.noreply.github.com>
2026-02-14 23:22:03 +0300

    feat(logging): implement central logger and configure manifest

---
+++ DVLD.Utilities/clsLogger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DVLD.Utilities
+{
+    public static class clsLogger
+    {
+        private const string _sourceName = "DVLD_App";
+
+        public static void LogError(string message) => _LogToEventLog(message, EventLogEntryType.Error);
+
+        public static void LogError(Exception ex)
+            => _LogToEventLog($"Message: {ex.Message}\n\nStack: {ex.StackTrace}", EventLogEntryType.Error);
+
+        public static void LogError(string customMessage, Exception ex)
+            => _LogToEventLog($"{customMessage}\n\nException: {ex.Message}\n\nStack: {ex.StackTrace}", EventLogEntryType.Error);
+
+        public static void Log(string message, EventLogEntryType type) => _LogToEventLog(message, type);
+
+        private static void _LogToEventLog(string message, EventLogEntryType type)
+        {
+            try
+            {
+                if (!EventLog.SourceExists(_sourceName))
+                {
+                    EventLog.CreateEventSource(_sourceName, "Application");
+                }
+                EventLog.WriteEntry(_sourceName, message, type);
+            }
+            catch { }
+        }
+    }
+
+}
